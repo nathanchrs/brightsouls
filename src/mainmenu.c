a750c04f76cafe25666788224b77ae1df48ddd5b
Jonathan Christopher <jonathan.christopher@outlook.com>
2016-10-30 23:01:15 +0700

    Added comments for main menu implementation

--- src/mainmenu.c
+++ src/mainmenu.c
@@ -11,11 +11,19 @@ void MainMenu_showSplashScreen() {
 	delay(2.0);
 }
 
+/* Tampilkan main menu di layar: resume game (jika isGameRunning false), new game, load game, save game, exit */
 void MainMenu_show(bool isGameRunning) {
 	clearScreen();
 	printf("\nShow main menu here, Ctrl+C to exit\n");
 	delay(0.5);
 }
+
+/* Setelah main menu tampil, baca pilihan:
+   - resume game (hanya bisa jika *isGameRunning false dan sudah ada nama user di gamestate): set *isGameRunning = true, *exitgame = false
+   - new game: baca input nama pemain, GameState_new(gameState, playerName), resume game
+   - load game: input nama saveFile, GameState_load(gameState, saveFile), cek validitas saveFile, resume game
+   - save game: pilih nama savefile, GameState_save(gameState, saveFile), *isGameRunning = false, *exitGame = false
+   - exit: *exitGame = true, *isGameRunning = false
 
 // void MainMenu_processInput(GameState *gameState, bool *isGameRunning, bool *exitGame) {
 
