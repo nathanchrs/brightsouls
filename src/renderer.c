4ba66164e825cbac37e2a71ad22ce97b3c8de720
ironlota <raydreww@gmail.com>
2016-11-26 16:59:57 +0700

    add credit renderer and additional save primitive, not yet all implemented

--- src/renderer.c
+++ src/renderer.c
@@ -2,6 +2,7 @@
 #include "skilltreerenderer.h"
 #include "arearenderer.h"
 #include "battlerenderer.h"
+#include "creditrenderer.h"
 
 void Renderer_render(FrameBuffer *fb, const GameState *gameState, const GameResources *gameResources) {
 	FrameBuffer_clear(fb);
@@ -12,6 +13,8 @@ void Renderer_render(FrameBuffer *fb, const GameState *gameState, const GameReso
 		SkillTreeRenderer_render(fb, gameState, &(gameResources->skillTree));
 	} else if (gameState->currentPhase == BATTLE) {
         BattleRenderer_render(fb, gameState, gameResources);
+    } else if (gameState->currentPhase == CREDITS) {
+        CreditRenderer_render(fb, gameState);
     }
 
 	FrameBuffer_output(fb, true);
